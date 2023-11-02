0258ed0c8e117dd032691049896993b54636e2e0
komesh26 <38267602+komesh26@users.noreply.github.com>
2023-11-02 20:51:12 +1300

    Feat: new project

---
+++ index.jsx
@@ -0,0 +1,101 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// app.get("/api/", (req, res) => {
+//   var resDate = new Date();
+//   res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
+// });
+
+app.get("/api/:date?", (req, res) => {
+  let input = req.params.date;
+
+  /*1.create variable for checking :  
+      a.isValidDate 
+        to check if input is string with valid date format -> 
+        if not valid date, it will return NaN 
+        example : 
+        -valid   : 2015-12-25, 
+        -invalid : 2015-02-31, 1451001600000
+  */
+  let isValidDate = Date.parse(input);
+
+  /*  b.isValidUnixNumber
+        to check if input is string with whole number(no symbol or character in the middle of input) -> 
+        it must be valid unix (source : https://benjaminsemah.com/build-timestamp-microservice)
+  */
+  let isValidUnixNumber = /^[0-9]+$/.test(input);
+
+  //  c.isEmpty to check there is nothing in input
+  let isEmpty = input == "" || input == null;
+
+  //3.create another variables used in if-else
+  let unix_output = 0;
+  let utc_output = "";
+
+  if (isValidDate) {
+    unix_output = new Date(input);
+    utc_output = unix_output.toUTCString();
+    // valueOf used for getting a variable back to primitive type
+    return res.json({ unix: unix_output.valueOf(), utc: utc_output });
+  } else if (isNaN(isValidDate) && isValidUnixNumber) {
+    unix_output = new Date(parseInt(input));
+    utc_output = unix_output.toUTCString();
+    return res.json({ unix: unix_output.valueOf(), utc: utc_output });
+  } else if (isEmpty) {
+    unix_output = new Date();
+    utc_output = unix_output.toUTCString();
+    return res.json({ unix: unix_output.valueOf(), utc: utc_output });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+app.get("/api/timestamp/", (req, res) => {
+  var resDate = new Date();
+  res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
+});
+
+// normal timestamp endpoint...
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  var reqString = req.params?.date_string;
+  var resDate;
+
+  console.log("timestamp", res, "req--->", req);
+  // check to see if the string is a unix timestamp (in this challenge we can just see if it contains a dash as the 5th character), and perform the conversion to an integer if necessary
+  if (!/^\d{4}-/.test(reqString)) reqString = parseInt(reqString);
+
+  resDate = new Date(reqString);
+  // this comparision is used to see if the date is a valid date, is there another way to do this?
+  if (resDate.getTime() !== resDate.getTime()) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
